f1d081577381702d0553d22d9355ecf2e1f01288
Anne Medina <carmelaanne.medina@gmail.com>
2024-11-08 12:22:41 +1030

    This uses VITE and ES where the side effect of ReactMde is not exporting the same thing from its own internal code as what is expected from using ES Modules but apparently, there isn't a need to create the R object. This fixed the issue of the application running in localhost

--- App.jsx
+++ App.jsx
@@ -1,103 +1,82 @@
-import React from "react"
-import Sidebar from "./components/Sidebar"
-import Editor from "./components/Editor"
-import Split from "react-split"
-import { nanoid } from "nanoid"
-import {
-    onSnapshot,
-    addDoc,
-    doc,
-    deleteDoc,
-    setDoc
-} from "firebase/firestore"
-import { notesCollection, db } from "./firebase"
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import Split from "react-split";
+import { onSnapshot, addDoc, doc, deleteDoc, setDoc } from "firebase/firestore";
+import { notesCollection, db } from "./firebase";
 export default function App() {
-    const [notes, setNotes] = React.useState([])
-    const [currentNoteId, setCurrentNoteId] = React.useState("")
-    
-    const currentNote =
-        notes.find(note => note.id === currentNoteId)
-        || notes[0]
-        
-    /**
-     * Challenge:
-     * 1. Add createdAt and updatedAt properties to the notes
-     *    When a note is first created, set the `createdAt` and `updatedAt`
-     *    properties to `Date.now()`. Whenever a note is modified, set the
-     *    `updatedAt` property to `Date.now()`.
-     * 
-     * 2. TBA
-     */
+  const [notes, setNotes] = React.useState([]);
+  const [currentNoteId, setCurrentNoteId] = React.useState("");
 
-    React.useEffect(() => {
-        const unsubscribe = onSnapshot(notesCollection, function (snapshot) {
-            const notesArr = snapshot.docs.map(doc => ({
-                ...doc.data(),
-                id: doc.id
-            }))
-            setNotes(notesArr)
-        })
-        return unsubscribe
-    }, [])
-    
-    React.useEffect(() => {
-        if (!currentNoteId) {
-            setCurrentNoteId(notes[0]?.id)
-        }
-    }, [notes])
+  const currentNote =
+    notes.find((note) => note.id === currentNoteId) || notes[0];
 
-    async function createNewNote() {
-        const newNote = {
-            body: "# Type your markdown note's title here"
-        }
-        const newNoteRef = await addDoc(notesCollection, newNote)
-        setCurrentNoteId(newNoteRef.id)
+  /**
+   * Challenge:
+   * 1. Add createdAt and updatedAt properties to the notes
+   *    When a note is first created, set the `createdAt` and `updatedAt`
+   *    properties to `Date.now()`. Whenever a note is modified, set the
+   *    `updatedAt` property to `Date.now()`.
+   *
+   * 2. TBA
+   */
+
+  React.useEffect(() => {
+    const unsubscribe = onSnapshot(notesCollection, function (snapshot) {
+      const notesArr = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setNotes(notesArr);
+    });
+    return unsubscribe;
+  }, []);
+
+  React.useEffect(() => {
+    if (!currentNoteId) {
+      setCurrentNoteId(notes[0]?.id);
     }
+  }, [notes]);
 
-    async function updateNote(text) {
-        const docRef = doc(db, "notes", currentNoteId)
-        await setDoc(docRef, { body: text }, { merge: true })
-    }
+  async function createNewNote() {
+    const newNote = {
+      body: "# Type your markdown note's title here",
+    };
+    const newNoteRef = await addDoc(notesCollection, newNote);
+    setCurrentNoteId(newNoteRef.id);
+  }
 
-    async function deleteNote(noteId) {
-        const docRef = doc(db, "notes", noteId)
-        await deleteDoc(docRef)
-    }
+  async function updateNote(text) {
+    const docRef = doc(db, "notes", currentNoteId);
+    await setDoc(docRef, { body: text }, { merge: true });
+  }
 
-    return (
-        <main>
-            {
-                notes.length > 0
-                    ?
-                    <Split
-                        sizes={[30, 70]}
-                        direction="horizontal"
-                        className="split"
-                    >
-                        <Sidebar
-                            notes={notes}
-                            currentNote={currentNote}
-                            setCurrentNoteId={setCurrentNoteId}
-                            newNote={createNewNote}
-                            deleteNote={deleteNote}
-                        />
-                        <Editor
-                            currentNote={currentNote}
-                            updateNote={updateNote}
-                        />
-                    </Split>
-                    :
-                    <div className="no-notes">
-                        <h1>You have no notes</h1>
-                        <button
-                            className="first-note"
-                            onClick={createNewNote}
-                        >
-                            Create one now
-                </button>
-                    </div>
+  async function deleteNote(noteId) {
+    const docRef = doc(db, "notes", noteId);
+    await deleteDoc(docRef);
+  }
 
-            }
-        </main>
-    )
+  return (
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={currentNote}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
+          <Editor currentNote={currentNote} updateNote={updateNote} />
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
 }
--- firebase.js
+++ firebase.js
@@ -1,14 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore"
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD_uXh8KfNe11S8qhfQuB_RAzIHrKFTOHE",
+//   authDomain: "react-notes-7a013.firebaseapp.com",
+//   projectId: "react-notes-7a013",
+//   storageBucket: "react-notes-7a013.appspot.com",
+//   messagingSenderId: "370854322953",
+//   appId: "1:370854322953:web:3e20fb09a3b7f61fc329d8"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD_uXh8KfNe11S8qhfQuB_RAzIHrKFTOHE",
-  authDomain: "react-notes-7a013.firebaseapp.com",
-  projectId: "react-notes-7a013",
-  storageBucket: "react-notes-7a013.appspot.com",
-  messagingSenderId: "370854322953",
-  appId: "1:370854322953:web:3e20fb09a3b7f61fc329d8"
+  apiKey: "AIzaSyATodcAc4v8KsJYyyEBL2VZRM6WvL1GGTA",
+  authDomain: "reactfirebase-8c57b.firebaseapp.com",
+  projectId: "reactfirebase-8c57b",
+  storageBucket: "reactfirebase-8c57b.firebasestorage.app",
+  messagingSenderId: "848530666005",
+  appId: "1:848530666005:web:105b0a01b0f755c991141c",
+  measurementId: "G-E0JGK6SP8L"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
