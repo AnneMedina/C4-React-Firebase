47aa0ea4932c231b8a23722407574c6231f6190b
Anne Medina <carmelaanne.medina@gmail.com>
2024-11-08 12:30:07 +1030

    More screenshots of note with new fields

--- App.jsx
+++ App.jsx
@@ -43,6 +43,8 @@ export default function App() {
   async function createNewNote() {
     const newNote = {
       body: "# Type your markdown note's title here",
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
     };
     const newNoteRef = await addDoc(notesCollection, newNote);
     setCurrentNoteId(newNoteRef.id);
